7eaec611d73137e2768049cc8ca091549c990643
Paszo <paszo2008@gmail.com>
2019-02-10 09:22:32 +0100

    [ADD] Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{'name': 'Library Book Borrowing',
+ 'description': 'Members can book the books from the library.',
+ 'author': 'Daniel Reis',
+ 'depends': ['library_member'],
+ 'data': [
+ 'security/ir.model.access.csv',
+ 'views/library_menu.xml',
+ 'views/checkout_view.xml',
+ ]
+}
