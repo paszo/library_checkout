85d1b375f06f45ea9b8a696b7d65c596823d6beb
Paszo <paszo2008@gmail.com>
2019-02-10 13:58:15 +0100

    [ADD] Added the wizard model

---
+++ wizard/checkout_mass_message.py
@@ -0,0 +1,10 @@
+from odoo import api, exceptions, fields, models
+
+class CheckoutMassMessage(models.TransientModel):
+    _name = 'library.checkout.massmessage'
+    _description = 'Send Message to Borrowers'
+    checkout_ids = fields.Many2many(
+        'library.checkout',
+        string='Checkouts')
+    message_subject = fields.Char()
+    message_body = fields.Html()
